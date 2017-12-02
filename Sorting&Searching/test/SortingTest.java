dc4707717711b32937928b0fb96b29677ada8bf8
Yifferpi <32341392+Yifferpi@users.noreply.github.com>
2017-12-02 12:12:42 +0100

    rewrote SortingTest for testing variable size of arrays

--- Sorting&Searching/test/SortingTest.java
+++ Sorting&Searching/test/SortingTest.java
@@ -1,22 +1,46 @@
 import static org.junit.Assert.*;
-
 import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.Test;
 
 
-public class SortingTest {
 
+public class SortingTest {
 	
+	final int ARRAY_LENGTH = 10000000;
+	
+	int[] input = new int[ARRAY_LENGTH];
+	int[] output = new int[ARRAY_LENGTH];
+	
+
 	
 	@Test
 	public void mergeSortTest() {
 		
-		int[] output = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-		int[] input = {2, 5, 3, 7, 4, 9, 8, 1, 0, 6};
+		createSortedArray(input);
+		createSortedArray(output);
+		shuffleArray(input, 100000000);
+		
 		Sorting.mergeSort(input, 0, input.length - 1);
 		assertTrue(Arrays.equals(output, input));
-
-
+	}
+	
+	public static void createSortedArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
+		}
+	}
+	
+	public static void shuffleArray(int[] array, int switches) {
+		Random random = new Random();
+		for (int i = 0; i < switches; i++) {
+			int e1 = random.nextInt(array.length);
+			int e2 = random.nextInt(array.length);
+			
+			int temp = array[e1];
+			array[e1] = array[e2];
+			array[e2] = temp;
+		}
 	}
 }
